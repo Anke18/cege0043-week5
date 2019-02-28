c14c005385c7661ac7d50b38484570cab36689b5
Anke18 <uczlaw3@ucl.ac.uk>
2019-02-28 07:17:44 -0500

    Modify some error and the code could work

--- uczlaw3/platforms/browser/www/js/testAJAX.js
+++ uczlaw3/platforms/browser/www/js/testAJAX.js
@@ -3,26 +3,27 @@ var xhrNode;
 function callDivNodeJSChange()
 {
 	xhrNode = new XMLHttpRequest();
-	var url = "http://developer.cege.ucl.ac.uk:" + httpPortNumber;
+	var filename = document.getElementById("filename").value;
+	var url = "http://developer.cege.ucl.ac.uk:" + httpPortNumber + '/' + filename;
 	xhrNode.open("GET", url, true);
 	xhrNode.onreadystatechange = processDivNodeJSChange;//no () here
 	// I use chrome??
-	try{
-		xhrNode.setRequestHeader("Content-Type", "application/x-www-formurlencoded");
-	}
-	catch(e) { }
+	//try{
+	//	xhrNode.setRequestHeader("Content-Type", "application/x-www-formurlencoded");
+	//}
+	//catch(e) { }
 	xhrNode.send();
 }
 
 function processDivNodeJSChange()
 {
 	if (xhrNode.readyState < 4) // while waiting response from server
-		document.getElementById('ajaxtext').innerHTML = "Loading...";
+		document.getElementById('ajaxtest').innerHTML = "Loading...";
 	else if (xhrNode.readyState === 4)
 	{
 		// 4 = Response from server has been completely loaded
 		if (xhrNode.status > 199 && xhrNode.status < 300)
 		// http status between 200 to 299 are all successful
-		document.getElementById('ajaxtext').innerHTML = xhrNode.responseText;
+		document.getElementById('ajaxtest').innerHTML = xhrNode.responseText;
 	}
 }
