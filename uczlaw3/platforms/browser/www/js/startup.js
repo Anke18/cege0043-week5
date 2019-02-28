3332d416c32a6c77815bf246751dbf2f5bd45ba7
Anke18 <uczlaw3@ucl.ac.uk>
2019-02-28 18:56:53 -0500

    Modify index and add createform1 html file

--- uczlaw3/platforms/browser/www/js/startup.js
+++ uczlaw3/platforms/browser/www/js/startup.js
@@ -4,9 +4,15 @@ function trackAndCircle(){
 	addPointLinePoly();
 	getEarthquakes();
 	getPort();
+	loadW3HTML();
 }
 
 function startup(){
 	document.addEventListener('DOMContentLoaded',
 	function(){getPort();}, false);
+}
+
+function loadW3HTML()
+{
+	w3.includeHTML();
 }
